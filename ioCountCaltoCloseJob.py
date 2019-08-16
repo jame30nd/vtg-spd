3b86e4b3398b3129cac1683a89945f75b8a580e5
Jamebond <tanakorn.niamchaona@gmail.com>
2019-08-16 15:51:45 +0700

    update program

--- ioCountCaltoCloseJob.py
+++ ioCountCaltoCloseJob.py
@@ -8,6 +8,13 @@ import array
 filename = 'SPD-xx'
 fileLog = 'SPD-LOG-xx'
 
+compensationMM = 43.81
+compensationYard = 0.0479111986
+
+ioConnectMount = 'sudo mount.cifs //172.16.54.17/iotData /media/AIP -o rw,uid=pi,password=eseuser'
+pathLog = '/home/pi/VTG/SPD-LOG.csv'
+osUnMount = 'sudo umount /media/AIP'
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(18, GPIO.IN)
 GPIO.setup(23, GPIO.IN)
@@ -33,13 +40,26 @@ yard = 0
 c = 285.00
 p = 600.00
 df =pd.DataFrame({'mm':[]})
-mmLog = array.array('d',(0 for i in range(0,10)))
 i = 0
+
+def writeFile (datafram):
+    while (os.path.ismount('/media/AIP') == False):
+        os.system(ioConnectMount)
+    try:
+        print('WriteFile CSV')
+        datafram.to_csv('/media/AIP/{0}.csv'.format(filename), encoding="iso-8859-1", index=False)
+        #f.write('\n{0:.4f},{1:.4f},{2}'.format(sumLog, yardLog, time.time()))
+        time.sleep(0.3)
+    except IOError:
+        print("Error")
+        pass
+        #f.close()
 
 # 1 - 3
 #os.system('sudo mount.cifs //172.16.54.17/iotData /media/AIP -o rw,uid=pi,password=eseuser')
 # 4 - 6
 #os.system('sudo mount.cifs //172.16.54.14/iotData /media/AIP -o rw,uid=pi,password=eseuser')
+
 while True :
         input_value = GPIO.input(18)
         in2 = GPIO.input(23)
@@ -61,69 +81,48 @@ while True :
                 buffer = input_value
             elif(in2 == 1 and in3 == 1 and in5 == 1  and in6 == 1 and in7 == 1 and in8 == 0 and in8buffer == 1 and in9 == 1): # Cut Spreadin 2-6
             #elif (in2 == 0 and in3 == 1 and in5 == 1 and in6 == 1 and in7 == 1 and in8 == 0 and in8buffer == 1 and in9 == 1):  # Cut Spreading 1
-                os.system('sudo mount.cifs //172.16.54.17/iotData /media/AIP -o rw,uid=pi,password=eseuser')
-                f = open('/home/pi/VTG/SPD-LOG.csv','a')
+                os.system(ioConnectMount)
+                #f = open(pathLog,'a')
                 time.sleep(0.3)
                 sumLenght = sumLenght+mm
-                sumLog = mm
-                yard = sumLenght*0.00109361
-                yardLog  = mm *0.00109361
+                #sumLog = mm
+                yard = sumLenght*0.00109361 + compensationMM
+                #yardLog  = mm *0.00109361 + compensationYard
                 counter = 0
-                #mm = 0
-                print("I/O Cuting  : sumLenght (mm)",sumLog," sumLenght (yard)",yardLog)
+                #print("I/O Cuting  : sumLenght (mm)",sumLog," sumLenght (yard)",yardLog)
                 datafram = pd.DataFrame({'YD': ['{0:.4f}'.format(yard)], 'MT': ['{0:.4f}'.format(sumLenght/1000)]})
-                try:
-                    print('WriteFile CSV')
-                    datafram.to_csv('/media/AIP/{0}.csv'.format(filename),encoding="iso-8859-1",index=False)
-                    f.write('\n{0:.4f},{1:.4f},{2}'.format(sumLog,yardLog,time.time()))
-                    time.sleep(0.3)
-                except IOError :
-                    print("Error")
-                    pass
-                os.system('sudo umount /media/AIP')
-                sumLog = 0
-                yardLog = 0
+                writeFile(datafram)
+                os.system(osUnMount)
+                #sumLog = 0
+                #yardLog = 0
                 mm= 0
-                f.close()
-                #yard = 0
-                #sumLenght = 0
+                #f.close()
                 time.sleep(2)
             elif(in2 == 1 and in3 == 1 and in4 == 0 and in4buffer == 1 and in5 == 1 and in6 == 0 and in7 == 1 and in8 == 1 and in9 == 1):   # No material
-                os.system('sudo mount.cifs //172.16.54.17/iotData /media/AIP -o rw,uid=pi,password=eseuser')
-                f = open('/home/pi/VTG/SPD-LOG.csv', 'a')
+                os.system(ioConnectMount)
+                #f = open(pathLog, 'a')
                 time.sleep(0.3)
                 sumLenght = sumLenght+mm
-                yard = sumLenght*0.00109361
-                sumLog = mm
-                yardLog = mm *0.00109361
+                yard = sumLenght*0.00109361 + compensationMM
+                #sumLog = mm
+                #yardLog = mm *0.00109361 + compensationYard
                 mm = 0
                 print("No Material ","sumLenght (mm)",sumLenght," sumLenght (yard)",yard)
                 datafram = pd.DataFrame({'YD': ['{0:.4f}'.format(yard)], 'MT': ['{0:.4f}'.format(sumLenght / 1000)]})
-                try:
-                    print('WriteFile CSV')
-                    datafram.to_csv('/media/AIP/{0}.csv'.format(filename),encoding="iso-8859-1",index=False)
-                    f.write('\n{0:.4f},{1:.4f},{2}'.format(sumLog,yardLog,time.time()))
-                    time.sleep(0.3)
-                except IOError :
-                    print("Error")
-                    pass
-                f.close()
+                writeFile(datafram)
                 counter = 0
-                sumLog = 0
-                yardLog = 0
-                f.close()
+                #sumLog = 0
+                #ardLog = 0
+                #f.close()
                 time.sleep(1)
-            # elif (in10 == 0 and closeJobBuffer == 1):  # CloseJob
-            #     # sumLenght = sumLenght+mm
-            #     yard = sumLenght * 0.00109361
-            #     print("Close Job ", "sumLenght (mm)", sumLenght, " sumLenght (yard)", yard)
-            #     mm = 0
-            #     counter = 0
-            #     sumLenght = 0
             closeJobBuffer = in10
             in4buffer = in4
             in8buffer = in8
         elif (in11 ==0 and in2 == 0 and in5 == 1 and in6 == 1) :
             mm= 0
             sumLenght = 0
-            print("5555")
+
+
+
+
+
